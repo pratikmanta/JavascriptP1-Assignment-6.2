a50ec79d857215b8a85e460cfc257a8b0b30d295
Pratik Manta <pratik.manta10@gmail.com>
2018-07-08 01:07:36 +0530

    JS Assignment submission

---
+++ Assignment-6.2/index.js
@@ -0,0 +1,30 @@
+// declaration of variables 
+var recipeName ="Creamy squash linguine";
+var preparationTime = 45;
+var recipeBy = "Jimmy Cutter";
+var isVegetarian = true;
+var calories = 223.7;
+
+// new variables for update
+var newPreparationTime = 60;
+
+// Recipe details 
+console.log("Recipe Details ");
+console.log("--------------");
+console.log(`Recipe Name:${recipeName}`);
+console.log("Preparation Time:" + preparationTime);
+console.log("Recipe By:" + recipeBy);
+console.log("is Vegetarian Dish:" + isVegetarian);
+console.log("Calories:" + calories);
+
+// Recipe details after update
+console.log("Recipe Details after update");
+console.log("--------------");
+console.log(`Recipe Name:${recipeName}`);
+console.log("Preparation Time:" + newPreparationTime);
+console.log("Recipe By:" + recipeBy + " George ");
+console.log("is Vegetarian Dish:" + isVegetarian);
+console.log("Calories:" + calories);
+
+// run with command node index
+
